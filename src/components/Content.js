ddd4072326a607227f2b4268fe8baa9076d71f8c
sumori <hoangthiphuonguyen1602@gmail.com>
2023-01-12 14:46:33 +0700

    add rank

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,12 +5,34 @@ import Topcard from "./Card/Topcard";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllRemindersAsync } from "../redux/Slices/ReminderSlice";
+import Axios from "axios";
 const Content = () => {
   const reminders = useSelector((state) => state.reminder.reminders);
   const dispatch = useDispatch();
+  let topThree = [];
   useEffect(() => {
     dispatch(getAllRemindersAsync());
   }, [dispatch]);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await Axios.get(
+        "https://localhost:5000/api/message/getTopThreeFriends",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      topThree = data.data.data;
+    }
+    fetchData();
+    // return () => {
+    //   // cleanup
+    //   setchatData({});
+    // }
+  });
+  console.log(topThree);
+  
   const allReminders = reminders?.map((reminder) => {
     return (
       <div style={{ display: "flex" }}>
