8062720d25c881a4fefc4be88f151a0a31688abc
arishuy <huybui19898@gmail.com>
2023-01-12 13:19:16 +0700

    u

--- src/components/Card/Friendcard.js
+++ src/components/Card/Friendcard.js
@@ -11,7 +11,7 @@ const Friendcard = (props) => {
       <div className="contact-avatar">
         <img
           className="avatar__image"
-          src="http://chiase24.com/wp-content/uploads/2022/02/tang-hap-hanh-anh-avatar-hai-haeac-nhan-la-ba_t-caea_i-1.jpg"
+          src={props.pic}
           alt="avatar"
         ></img>
       </div>
